7bdc257592f291d1b441533c305fd13583905c20
Nasir <mohammadnasirk53@gmail.com>
2024-03-11 16:58:04 +0530

    added circular loaading

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,19 +1,23 @@
 import {useEffect,useState} from 'react'
 import axios from 'axios'
 import PostCard from "../components/Card"
-
+import CircularProgress from '@mui/material/CircularProgress';
+import { Stack } from '@mui/material';
 export default function Cards() {
     const [data,setData] = useState([])
+    const [loading,setLoading] = useState(true)
     useEffect( () => {
         const fetch = async () => {
           const res = await axios.get('/api/posts/')
           setData(res.data)
+          setLoading(false)
         };
         fetch();    
       },[]);
     return (
         <>
-        {data.map( (post) => (<PostCard key={post._id} title={post.title} desc={post.desc} img={post.ImgUrl} id={post._id} likes={post.likes} createdAt={post.createdAt} userId={post.UserId}/>))}
+        {loading ? <Stack height={'400px'} alignItems={'center'} justifyContent={'center'}><CircularProgress color='secondary' sx={{justifySelf:'center' ,alignSelf:'center'}}/></Stack> :  data.map( (post) => (<PostCard key={post._id} title={post.title} desc={post.desc} img={post.ImgUrl} id={post._id} likes={post.likes} createdAt={post.createdAt} userId={post.UserId}/>))}
+       
         </>
     )
 }
--- client/src/components/Recommendation.jsx
+++ client/src/components/Recommendation.jsx
@@ -2,20 +2,23 @@ import {useEffect,useState} from 'react'
 import axios from 'axios'
 import PostCard from '../components/Card'
 import { Divider, Stack } from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress';
 
 export const Recommendation = () => {
     const [data,setData] = useState([])
+    const [loading,setLoading] = useState(true)
     const newdata = data.splice(0,4)
     useEffect( () => {
         const randomdata = async () => {
             const res = await axios.get('/api/posts')
             setData(res.data)
+            setLoading(false)
        };
         randomdata()
     },[])
   return (
     <Stack spacing={2}>
-    {newdata.map( (post) => (<PostCard key={post._id} title={post.title} desc={post.desc} img={post.ImgUrl} userId={post.UserId} createdAt={post.createdAt}/>))}
+    {loading ? <CircularProgress sx={{alignSelf:'center'}} color='secondary'/> : newdata.map( (post) => (<PostCard key={post._id} title={post.title} desc={post.desc} img={post.ImgUrl} userId={post.UserId} createdAt={post.createdAt}/>))}
     <Divider sx={{color:'black'}}/>
     </Stack>
   )
